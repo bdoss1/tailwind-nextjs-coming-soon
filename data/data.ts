9a6fc372f6f888974a30f10b7a6dd403d84e3402
Baron Doss <bdoss1@gmail.com>
2024-07-20 18:32:10 +0000

    init commit

--- data/data.ts
+++ data/data.ts
@@ -1,27 +1,27 @@
 const currentYear = new Date().getFullYear();
 const data = {
-    sitename: "Sandip Baikare Blog",
+    sitename: "Grayband Pro",
     sitetagline: "Something great is on the way! ✨ 🔥",
-    siteurl: "https://github.com/baikaresandip",
+    siteurl: "https://graybandpro.com",
     sitelogo: "",
     title: "Coming Soon!",
     description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
-    copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>`,
+    copyrightText: `Copyright © ${currentYear} | Design By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://www.barondoss.com">Baron Doss</a>`,
     socialIconsHeading: "Follow Us 📣",
     hideSubscribeForm: false, // make true to disable subscription form 
     socialIcons: [
         {
             icon: "facebook",
-            link: "https://www.facebook.com/sandeep.baikare/",
+            link: "https://www.facebook.com/graybandpro/",
         },
         {
             icon: "twitter",
-            link: "https://twitter.com/BaikareSandeep",
+            link: "https://twitter.com/Graybandpro",
         },
         {
-            icon: "linkedIn",
-            link: "https://www.linkedin.com/in/sandip-baikare/",
+            icon: "instagram",
+            link: "https://www.instagram.com/graybandpro/",
         },
     ],
     hide :{
