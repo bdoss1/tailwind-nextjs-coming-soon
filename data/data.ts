29ebbae81e33ca6e1b8ecf6e85c6f52dd805587f
Baron Doss <bdoss1@gmail.com>
2024-07-20 20:11:56 +0000

    removed demo data

--- data/data.ts
+++ data/data.ts
@@ -1,12 +1,12 @@
 const currentYear = new Date().getFullYear();
 const data = {
     sitename: "Grayband Pro",
-    sitetagline: "Something great is on the way! ✨ 🔥",
+    sitetagline: "",
     siteurl: "https://graybandpro.com",
     sitelogo: "",
     title: "Coming Soon!",
-    description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
-    newsletterheading: "Stay tuned for our live updates!",
+    description: "",
+    newsletterheading: "",
     copyrightText: `Copyright © ${currentYear} | Design By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://www.barondoss.com">Baron Doss</a>`,
     socialIconsHeading: "Follow Us 📣",
     hideSubscribeForm: false, // make true to disable subscription form 
